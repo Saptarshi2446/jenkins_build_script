e88b6266aea92c7be22b684cc6317b1fb5c7ab53
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2023-03-18 21:19:06 +0530

    Update data_upload.py

--- data_upload.py
+++ data_upload.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 
 # Set the database connection details
 conn = psycopg2.connect(
-    host='18.222.253.244',
+    host='3.141.170.249',
     port='5432',
     dbname='zabbix_db1',
     user='zabbixuser',
@@ -90,32 +90,3 @@ for month, dates in data_dict.items():
 cursor.close()
 conn.close()
 
-
-#
-# SECOND PART ::  MOVING TO FOLDERS BASED ON MONTHS
-#
-import os
-import string
-import shutil
-
-# Define the path where the CSV files are located
-csv_path = "C:\\Old laptop Copy\\Copy\\datateam\\Test\\New_folder\\History"
-
-# Define the path where the new folders will be created
-new_folder_path = "C:\\Old laptop Copy\\Copy\\datateam\\Test\\New_folder\\History\\Testing"
-
-# Loop through all files in the CSV directory
-for file_name in os.listdir(csv_path):
-    # Check if the file is a CSV file
-    if file_name.endswith(".csv"):
-        # Get the alphabetic characters in the file name
-        alphabetic_chars = "".join(filter(lambda c: c in string.ascii_letters, file_name))
-
-        # Create a new folder with the alphabetic characters in the file name
-        new_folder_name = os.path.join(new_folder_path, alphabetic_chars)
-        os.makedirs(new_folder_name, exist_ok=True)
-
-        # Move the CSV file to the new folder
-        old_file_path = os.path.join(csv_path, file_name)
-        new_file_path = os.path.join(new_folder_name, file_name)
-        shutil.move(old_file_path, new_file_path)
