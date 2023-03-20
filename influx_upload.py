089fc76f784ff67f47b3772a7816103f2cfab6ac
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2023-03-20 14:46:26 +0530

    Create influx_upload.py

---
+++ influx_upload.py
@@ -0,0 +1,57 @@
+#
+# PART THREE ::  UPLOAD TO INFLUX
+import os
+import pandas as pd
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+import glob
+
+# InfluxDB connection details
+client = InfluxDBClient(url="https://eastus-1.azure.cloud2.influxdata.com/orgs/7d380c053f102dc4", token="W5PsbtVPj8wi6P7B9J33oBAgha5RNVI-gk1fR3f-fkY5DoAeD_k8k21ebf7PWGIodsLHydP4MYm6CqwsTJ99mw==",org="7d380c053f102dc4")
+bucket = "Streamli1"
+
+# Write API instance
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# Parent directory containing subdirectories with CSV files
+csv_dir = "/var/lib/jenkins/workspace/Database_data"
+
+# Find all subdirectories in the parent directory
+subdirs = [entry for entry in os.scandir(csv_dir) if entry.is_dir()]
+
+# Loop through each subdirectory
+for subdir in subdirs:
+    print(f"Processing folder: {subdir.name}")
+    
+    # Find all CSV files in the subdirectory
+    csv_files = glob.glob(f"{subdir.path}/*.csv")
+
+    # Loop through each CSV file in the subdirectory
+    for csv_file in csv_files:
+        # Read CSV file into a pandas dataframe
+        df = pd.read_csv(csv_file, low_memory=False)
+
+
+        # Extract item name from file name (without .csv extension)
+        item_name = os.path.splitext(os.path.basename(csv_file))[0]
+
+        # Loop through each row in the dataframe
+        for row_index, row in df.iterrows():
+            field_value = row[2]
+            json_body = [
+                { 
+                    "measurement": subdir.name,
+                    "tags": {
+                        "Date": row[3],
+                        "Time": row[8],
+                        "Hostname": row[4],
+                        "Itemname": row[7],
+                        "Host_Group": row[5],
+                    },
+                    "fields": {
+                        "value": field_value,
+                    }
+                } 
+            ]
+            print(json_body)
+            write_api.write(bucket=bucket, org="7d380c053f102dc4", record=json_body)
