f97e55c9265ca431b93cbea26834afd8c15d0f18
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2023-03-21 23:15:33 +0530

    Update influx_new.py

--- influx_new.py
+++ influx_new.py
@@ -11,7 +11,7 @@ bucket = "Streamli1"
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-file_path = r'/var/lib/jenkins/workspace/Database_data2/__ Space utilization_Zabbix-Server-Active.csv'
+file_path = r'/var/lib/jenkins/workspace/Database_data2/Mar-2023_15-03-2023.csv'
 
 csvReader = pd.read_csv(file_path)
 
