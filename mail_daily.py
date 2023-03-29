ac86a44a21e9c498637aea49bcc80d156bd451fb
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2023-03-29 14:16:23 +0530

    Add files via upload

---
+++ mail_daily.py
@@ -0,0 +1,46 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+# Your Mailjet API credentials
+#mailjet_api_key = 'e907b2941debdb6e56a9db0e350fd07f'
+#mailjet_api_secret = '32c01d1b750667c0591b7585d1d67540'
+mailjet_api_key = '547fa6ae837634b0e2260b5d95aaf795'
+mailjet_api_secret = 'bc6be0bbf4faa323e695313e7340a0a1'
+
+# Recipient email
+to_email = 'saptarshi.r@paramaah.com'
+
+# Create message
+msg = MIMEMultipart()
+msg['From'] = 'sachinjayaprakash07@gmail.com'
+msg['To'] = to_email
+msg['Subject'] = 'Test Email with Attachment'
+
+body = 'Hello, this is a test email from Python using Mailjet with an attachment!'
+msg.attach(MIMEText(body, 'plain'))
+
+# Attach file
+filename = 'C:\\Old laptop Copy\\Copy\\datateam\\Test\\New_folder\\pdf\\__ Free inodes in %_Zabbix-Server-Active (1).csv'
+attachment_path = os.path.join(os.getcwd(), filename)
+with open(attachment_path, 'rb') as attachment:
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename={filename}')
+    msg.attach(part)
+
+# Connect to Mailjet SMTP server and send email
+smtp_server = 'in-v3.mailjet.com'
+smtp_port = 587
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()
+server.login(mailjet_api_key, mailjet_api_secret)
+text = msg.as_string()
+server.sendmail(msg['From'], to_email, text)
+server.quit()
+
+print('Email sent successfully with attachment!')
